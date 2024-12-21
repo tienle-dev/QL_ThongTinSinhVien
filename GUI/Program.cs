2638341492e745c66c034a014ea17af33791d076
Trannam211 <trannamforgot211@gmail.com>
2024-12-21 13:37:36 +0700

    nam commit lan 2

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -16,7 +16,7 @@ namespace GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmGiangvien());
+            Application.Run(new Form1());
         }
     }
 }
